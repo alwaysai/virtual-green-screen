0a3763ca82bd17576937c8cee36a9d65109b9d03
Eric VanBuhler <eric@alwaysai.co>
2024-07-23 10:27:58 -0700

    Update model links

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ of a frame and either blur the background or replace the background with an
 image.
 
 To change the computer vision model, follow this guide:
-https://dashboard.alwaysai.co/docs/application_development/changing_the_model.html
+https://docs.alwaysai.co/application_development/application_configuration.html#change-the-computer-vision-model
 To change the engine and accelerator, follow this guide:
 https://docs.alwaysai.co/application_development/application_configuration.html#change-the-engine-and-accelerator
 """
